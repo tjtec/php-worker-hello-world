efd265f5549b0efbfb19dbf5c9a1421bc603a308
Tim <62452923+tjtec@users.noreply.github.com>
2025-08-26 04:37:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
-function generateRandomString($length = 12) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{}|;:,.<>?';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    
-    return $randomString;
-}
-
-// Generate and echo a random string
-echo "Random String: " . $randomString;
-?>
+echo "Randomstring";
