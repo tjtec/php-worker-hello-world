e1e8c72f4aebe02cca8488c662d2b8414e0f1b42
Tim <62452923+tjtec@users.noreply.github.com>
2025-08-26 04:14:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
-
-addEventListener("fetch", function ($event) {
-    $event->respondWith(handleRequest($event->request));
-});
-
-function handleRequest($request) {
-    return new Response("PHP Worker hello world", [
-        "headers" => [ "content-type" => "text/plain" ]
-    ]);
+function generateRandomString($length = 12) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{}|;:,.<>?';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    
+    return $randomString;
 }
+
+// Generate and echo a random string
+echo "Random String: " . $randomString;
+?>
